3e96b1e0cbb30f38a821e4e289e88bcd1beaed39
padhy12 <padhymanisha1997@gmail.com>
2025-02-25 17:27:32 +0530

    added footer section in all pages

---
+++ src/app/Components/footer-bcb/footer-bcb.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-bcb',
+  standalone: false,
+  templateUrl: './footer-bcb.component.html',
+  styleUrl: './footer-bcb.component.css'
+})
+export class FooterBCBComponent {
+
+}
